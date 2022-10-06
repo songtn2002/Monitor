b83c8b76c797955f9bc34133c9d5aec13a03c253
songtn2002 <1936535038@qq.com>
2022-10-06 16:11:57 +0800

    1006b cron job finished

--- cron_script.sh
+++ cron_script.sh
@@ -6,10 +6,10 @@ echo "$process"
 if [ "$process" = "" ];
 then
 echo "no such process to kill as server.py, restart server"
-screen python3 server.py
+screen -d -m python3 Monitor/server.py
 else
 kill $process
 echo "process killed"
 sleep 2
-screen python3 server.py
+screen -d -m python3 Monitor/server.py
 fi
